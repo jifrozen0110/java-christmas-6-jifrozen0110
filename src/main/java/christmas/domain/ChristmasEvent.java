6d76ee1d2d5a1d74394378fc31c29b390689b5fd
jifrozen0110 <answldjs1836@gmail.com>
2023-11-14 20:32:11 +0900

    refactor(domain.event) : 크리스마스 이벤트 boolean 메서드 리팩토링

--- src/main/java/christmas/domain/ChristmasEvent.java
+++ src/main/java/christmas/domain/ChristmasEvent.java
@@ -4,29 +4,23 @@ import christmas.common.consts.SystemConst;
 import java.time.LocalDate;
 
 public class ChristmasEvent implements Event {
-    public static final int EVENT_EXECUTION_MONEY = 10_000;
     private static final String NAME = "크리스마스 디데이 할인";
     private static final LocalDate START_DAY = LocalDate.of(SystemConst.CURRENT_YEAR, SystemConst.CURRENT_MONTH,
             SystemConst.START_DAY);
     private static final LocalDate END_DAY = LocalDate.of(SystemConst.CURRENT_YEAR, SystemConst.CURRENT_MONTH,
             SystemConst.CHRISTMAS_DAY);
+    public static final int EVENT_EXECUTION_MONEY = 10_000;
     private static final int BASE_DISCOUNT = 1_000;
     private static final int DAILY_INCREMENT = 100;
 
     @Override
     public boolean validatePrice(Orders orders) {
-        if (orders.getTotalPrice() >= EVENT_EXECUTION_MONEY) {
-            return true;
-        }
-        return false;
+        return orders.getTotalPrice() >= EVENT_EXECUTION_MONEY;
     }
 
     @Override
     public boolean isValidDay(final LocalDate localDate) {
-        if (localDate.isAfter(START_DAY) && localDate.isBefore(END_DAY)) {
-            return true;
-        }
-        return false;
+        return !localDate.isBefore(START_DAY) && !localDate.isAfter(END_DAY);
     }
 
     @Override
